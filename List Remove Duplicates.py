2dc6bcfee033365997c0c025a05d01e38e0a20c3
Zeynep Alper <131172906+Zynplpr@users.noreply.github.com>
2023-04-23 14:27:31 +0200

    Update List Remove Duplicates.py

--- List Remove Duplicates.py
+++ List Remove Duplicates.py
@@ -14,3 +14,14 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+# or
+
+a = [2,3,7,3,2,6,8,3,5,234,46,765,2,34,89,567,234,63,45,653,34,23,556,234]
+numbers = set()
+
+for number in a:
+    numbers.add(number)
+print(numbers)
+
+

23d37d897a39d4ce8c65335afc2e72b50bd26fa7
Zeynep Alper <131172906+Zynplpr@users.noreply.github.com>
2023-04-23 14:25:53 +0200

    Create List Remove Duplicates.py

---
+++ List Remove Duplicates.py
@@ -0,0 +1,16 @@
+# https://www.practicepython.org/exercise/2014/05/15/14-list-remove-duplicates.html
+
+a = [2,3,7,3,2,6,8,3,5,234,46,765,2,34,89,567,234,63,45,653,34,23,556,234]
+def duplist(list_name):
+    new_list = list()
+    for number in list_name:
+        if number not in new_list:
+            new_list.append(number)
+    return new_list
+
+def main():
+    b = duplist(a)
+    print(b)
+
+if __name__ == "__main__":
+    main()
